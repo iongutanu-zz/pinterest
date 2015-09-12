40287375ae68ac3f8483d466a6f259b5d7267b65
Ion Gutanu <ion.gutanu@gmail.com>
2015-09-12 21:50:46 +0000

    Implemented devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,9 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+	# Required for Heroku
+	config.action_mailer.default_url_options = { host: 'omr-pinterest.herokuapp.com', port: 3000 }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
