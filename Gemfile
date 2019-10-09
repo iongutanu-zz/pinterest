b46f44f20c1b200ee8033caa2d0ee1eb015793f1
Ion Gutanu <igutanu@gmail.com>
2019-10-09 17:11:23 +0300

    Added Dockerfile to use inside docker

--- Gemfile
+++ Gemfile
@@ -33,10 +33,11 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'sqlite3', '~> 1.3.0'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
-	gem 'sqlite3', '~> 1.3.0'
 end
 
 group :development do
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,7 +6,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` on Rails 4+ applications as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = 'ff2fd59ce8aa9407d0908f0c8a1d83582e51b368e1200047f042c69f92ad42ce79d9a67783da8605555998c3f378827ad8634f1051ca9fcb9371d7ad48447253'
+  config.secret_key = ENV['DEVISE_SECRET_KEY']
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
